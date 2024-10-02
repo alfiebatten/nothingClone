779d091f84c836388a7819a6bfdbf79a88ba60c6
hinshey <hinsheycnft@gmail.com>
2024-10-02 15:24:33 +0100

    removing unused useref from cursor

--- nothing-clone/src/components/Cursor.js
+++ nothing-clone/src/components/Cursor.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
